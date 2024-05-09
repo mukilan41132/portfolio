978a11c0d4467f087aa410c6094b72a284e7e54c
mukilan <mukil.acumen@gmail.com>
2024-05-09 19:14:22 +0530

    ui changes

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import './App.css';
+function Header() {
+
+
+
+    return (
+        <>
+
+
+            <nav className="navBar">
+                <ul className="nav_flex">
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Skills</a></li>
+                    <li><a>Experience</a></li>
+                    <li><a>Contact me</a></li>
+                </ul>
+            </nav>
+
+
+        </>
+    );
+}
+
+export default Header;
