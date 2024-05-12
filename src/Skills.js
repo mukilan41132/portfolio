dd21265fef6c41c77eac63fc6774518931a57b74
mukil <rmukilan2002@mail.com>
2024-05-12 19:05:58 +0530

    im

--- src/Skills.js
+++ src/Skills.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from '../src/Header';
 import TextTypefrom from "./Typingeffect"
+import javascript from "./im/"
 function Skills() {
 
 
