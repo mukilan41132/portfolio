90b763049c2a7eeec749d57f22032d3a9d0b6f01
Mukilan <rmukilan2002@gmail.com>
2024-08-16 22:52:28 +0530

    deploy

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,5 @@
 import '../styles/App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 
 function About() {
--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,17 +1,16 @@
 import '../styles/App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import dataskill from '../Assets/Data/Dataskills.json';
 
 function Skills() {
-  const [Dataskill, setDataskill] = useState(dataskill);
 
-
+  const Dataskill = dataskill
   return (
     <>
       <div className='container'>
         {Dataskill.map((Skill, index) => (
           <div className='container_card' key={index}>
-            <img className='img_style' src={Skill.img}  />
+            <img className='img_style' src={Skill.img} />
             <p className='fsw'>{Skill.title}</p>
           </div>
         ))}
